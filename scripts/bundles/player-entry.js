a978941554f6c9af5662ee755fdb3accebcc4680
Intentionally Incomplete <alwaysinarush@protonmail.ch>
2025-11-12 22:48:09 -0800

    Removed secrets from continue rules

--- scripts/bundles/player-entry.js
+++
@@ -1,8 +0,0 @@
-// Auto-generated bundle entry for player
-import '../modules/feature-player.js';
-import '../modules/feature-stack.js';
-import '../modules/feature-video-overlay.js';
-import '../modules/feature-video-enhancements.js';
-import '../modules/feature-ambient.js';
-import '../modules/feature-pip.js';
-import '../modules/feature-resize.js';
